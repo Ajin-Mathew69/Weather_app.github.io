ade608dadb4f717823496d14a4497bd9112e2f07
Ajin-Mathew69 <ajin.mathewfsd@gmail.com>
2023-01-13 23:59:46 +0530

    final commit

---
+++ src/Components/Feels.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Feel(props) {
+  return (
+    <div className="max-w-[250px] lg:max-w-[400px] w-full h-[50px] lg:h-[50px] bg-slate-600 hover:bg-slate-900 group rounded-lg flex justify-around items-center mb-5 lg:mb-0">
+      <h1 className="text-2xl group-hover:text-4xl text-white  group-hover:font-extrabold">
+        <span className="text-lg">Feels-like:</span>
+        {props.city ? props.city.main.feels_like.toFixed(0) : ""}°C
+      </h1>
+    </div>
+  );
+}
+
+export default Feel;
